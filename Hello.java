837d7c45174424400b377a05e0a6fb737eb02d36
kaollie74 <kaollie74@gmail.com>
2019-10-18 15:20:26 -0500

    exploring with variables in java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,36 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+// Variable
+  // 1. "int" is a number without decimal points.
+
+// Declaration Statement
+  // - Used to define a varaible by indicting the data type, the name, and to set the variable to a certain value.
+
+// EXPRESSION 
+  //  - is a construct that evaluates to a singel value.
+
+// String Literal
+  // - Any sequence of characters surrounded by double quotes
 
 public class Hello {
 
   public static void main(String[] args) {
-    String x = "Hello Kyle.";
-    System.out.println(x);
+    
+    System.out.println("Hello Kyle");
+
+    int myFirstNumber = 5;
+    int mySecondNumber = 5 * 5;
+    System.out.println(myFirstNumber);
+    System.out.println(mySecondNumber);
+
+    int myThirdNumber = 12;
+    int myFourthNumber = 6; 
+     System.out.println(myThirdNumber);
+      System.out.println(myFourthNumber);
+
+
+
+    
 
   }
 }
